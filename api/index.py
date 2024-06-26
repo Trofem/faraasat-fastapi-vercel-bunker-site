73cb72b620cdaa8df04b13e66e732977cba2595b
SatCat <39083134+SatCat@users.noreply.github.com>
2024-06-26 17:13:18 +1100

    kv envs

--- api/index.py
+++ api/index.py
@@ -8,11 +8,18 @@ import redis
 app = FastAPI()
 
 KV_USERNAME = os.environ.get('KV_USERNAME')
+KV_PASS = os.environ.get('KV_PASS')
+KV_HOST = os.environ.get('KV_HOST')
+KV_PORT = os.environ.get('KV_PORT')
 
 @app.get("/")
 async def root():
     utc = datetime.utcnow()
     return {"GMT+11 time": format(utc+timedelta(hours=11))}    # make GMT+11
+
+@app.get("/r")
+async def root():
+    return {"redis_host": f'redis://{KV_HOST}:{KV_PORT}'}    
 
 
 @app.get("/html", response_class=HTMLResponse)
@@ -25,4 +32,4 @@ async def root():
         <body>
             <h1>Look me! HTMLResponse!</h1>
         </body>
-    </html> """+'@python '+str(python_formatted_version)+str(KV_USERNAME)
+    </html> """+'@python '+str(python_formatted_version)
