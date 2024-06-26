cfa1cad4a57c77b8b83fa5d5b1420a457909af87
SatCat <39083134+SatCat@users.noreply.github.com>
2024-06-26 11:28:47 +1100

    python_formatted_version

--- api/index.py
+++ api/index.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from sys import version as python_formatted_version
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/html", response_class=HTMLResponse)
+async def root():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look me! HTMLResponse!</h1>
+        </body>
+    </html> """+str(python_formatted_version)
