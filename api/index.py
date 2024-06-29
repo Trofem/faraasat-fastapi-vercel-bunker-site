2f11e1102d3b79e38a549c745384c5282b3e840c
SatCat <39083134+SatCat@users.noreply.github.com>
2024-06-29 21:48:21 +1100

    add POST

--- api/index.py
+++ api/index.py
@@ -25,24 +25,28 @@ async def root():
     utc = datetime.utcnow()
     return {"GMT+11 time": format(utc+timedelta(hours=11))}    # make GMT+11
 
-@app.get("/r")   # <host>/r?add=value to add
+@app.get("/r")   # GET  <host>/r?add=value to add
 async def r_add(request: Request):
     params = request.query_params
     pop = None
     if 'add' in params:
         r.lpush('list_val', str(params['add']))   # insert at list begin
-        r.ltrim('list_val', 0, 6)                 # save only first 7 elements
-    return {"redis_values": [i.decode("utf-8") for i in r.lrange('list_val',0,10)] }    
+        r.ltrim('list_val', 0, 20)                 # save only first 7 elements
+    return {"redis_values": [i.decode("utf-8") for i in r.lrange('list_val',0,21)] }    
+
+@app.post("/r")   
+async def r_post_add(request: Request):
+    if 'add' in request.query_params:
+        add_value = request.query_params.get('add')
+        r.lpush('list_val', str(add_value))         # insert at list begin
+        r.ltrim('list_val', 0, 20)                  # save only first 7 elements
+    return {"redis_values": [i.decode("utf-8") for i in r.lrange('list_val',0,21)] }    
 
 
 @app.get("/html", response_class=HTMLResponse)
 async def root():
     return """
     <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look me! HTMLResponse!</h1>
-        </body>
+        <head><title>Some HTML in here</title> </head>
+        <body><h1>Look me! HTMLResponse!</h1></body>
     </html> """+'@python '+str(python_formatted_version)
