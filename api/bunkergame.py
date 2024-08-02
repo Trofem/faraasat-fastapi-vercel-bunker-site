16e79e00ed305b3fa256f414ac621d822f438d03
trofim.al <127919576+Trofem@users.noreply.github.com>
2024-08-02 22:23:59 +1100

    major update (backend only)

--- api/bunkergame.py
+++ api/bunkergame.py
@@ -1,33 +1,51 @@
+from jinja2 import Environment, FileSystemLoader
 import os
 import random
 import json
 
 
-class Bunker:
-    directory:str = os.path.abspath(os.getcwd()) + "/api/cardFiles/"
-    parametreList:list[str]
-    cardType:str
+HTMLdirectory = os.path.abspath(os.getcwd()) + "/RandomIdeas/Bunker/html/"
 
-    def CreateBunkerCards(self, Type:str) -> None:
+jinja_env = Environment(loader=FileSystemLoader(HTMLdirectory))
+
+
+
+bunker_building_template = jinja_env.get_template('bunkerBuilding.html')
+
+bunker_character_template = jinja_env.get_template('bunkerCharacter.html')
+
+class Bunker:
+    directory:str = os.path.abspath(os.getcwd()) + "/RandomIdeas/Bunker/cardFiles/"
+    parametreList:list[str]
+    cardType:str #temporary value of created card type
+    
+    def __init__(self,card:str,count:int=1):
+        cards_count:int = count #count of cards needed to generate
+        cardType = card
+        self.CreateBunkerCards(cardType, cards_count)
+
+    def __str__(self) -> str:
+        global cardType
+        print(f"мы возращаем {self.cards_count}")
+        print("empty string in file! " + self.cardType) if "" in self.parametreList else ...
+        self.value:list[str] = random.sample(self.parametreList, self.cards_count)
+        return ", ".join(self.value) if self.value != '[]' else "emptyString while random choice. Сообщите разработчику об ошибке"
+    
+    def CreateBunkerCards(self, Type:str,count=1) -> None:
+        self.cards_count = count
         self.cardType = Type
         filepath:str = os.path.join(self.directory, f"Cards_{self.cardType}.txt")
-        #print("\n"+filepath,end="... ")
+        
         if os.path.exists(filepath):
             print(f"{self.cardType} Exist.\n")
 
+            #в текстовых файлах не должно быть двойных кавычек, где либо, важно
             with open(filepath, "r") as file:
                 self.parametreList = file.read().split("\n")
         else:
             print(f"{self.cardType} is not exist.")
             self.parametreList = ["Error"]
     
-    def __init__(self,cardType:str):
-        self.CreateBunkerCards(cardType)
-    
-    def __str__(self) -> str:
-        print("empty string in file! " + self.cardType) if "" in self.parametreList else ...
-        self.value = random.choice(self.parametreList)
-        return self.value if self.value != '' else "emptyString while random choice. Сообщите разработчику об ошибке"
 
 def letOrGod(year: int) -> str:
     if year == 1:
@@ -42,6 +60,7 @@ def letOrGod(year: int) -> str:
             return "года"
         else:
             return "лет"
+
 def AgeGenerator(firstAge:int=1, lastAge:int=99, decimalGeneration:bool=False) -> float:
     value = random.randint(firstAge,lastAge) #рандомное число в радиусе
 
@@ -50,11 +69,11 @@ def AgeGenerator(firstAge:int=1, lastAge:int=99, decimalGeneration:bool=False) -
 
     return value
 
-bunkerCharacterId = 0 #since starting site
+bunkerId = 0 #since starting site
 
 def CreateRandomCharacter(isJson:bool=False) -> str:
-    global bunkerCharacterId
-    bunkerCharacterId += 1
+    global bunkerId
+    bunkerId += 1
     gender = "Мужчина" if random.random() <= 0.504 else "Женщина"
     age = AgeGenerator(14,99)
 
@@ -62,17 +81,17 @@ def CreateRandomCharacter(isJson:bool=False) -> str:
 
     hobby_exp = AgeGenerator(1, round(age/3), decimalGeneration=True)
 
-    profession =     str(Bunker("Profession")).capitalize()
-    fear =           str(Bunker("Fear")).capitalize()
-    hobby =          str(Bunker("Hobby")).capitalize()
-    characteristic = str(Bunker("Characteristic")).capitalize()
-    baggage =        str(Bunker("Baggage")).capitalize()
-    personInfo =     str(Bunker("PersonInfo")).capitalize()
+    profession =     Bunker("Profession")
+    fear =           Bunker("Fear")
+    hobby =          Bunker("Hobby")
+    characteristic = Bunker("Characteristic")
+    baggage =        Bunker("Baggage")
+    personInfo =     Bunker("PersonInfo")
 
     if isJson:
         json_file_value = f'''
         {{
-            "id": {bunkerCharacterId},
+            "id": {bunkerId},
             "gender": "{gender}",
             "age": {age},
             "profession": "{profession}",
@@ -87,32 +106,37 @@ def CreateRandomCharacter(isJson:bool=False) -> str:
         '''
         return json.loads(json_file_value)
         
-
-    else:
-        return f"""
-    <html>
-    <body>
-    <ul>
-        <li><p>Пол:             {gender},</p></li>
-        <li><p>Возраст:         {age},</p></li>
-
-        <li><p>Профессия:       {profession},</p></li>
-        <li><p>Опыт работы:     {profession_exp} {letOrGod(profession_exp)},</p></li>
-
-        <li><p>Хобби:           {hobby},</p></li>
-        <li><p>Опыт хобби:      {hobby_exp} {letOrGod(hobby_exp)},</p></li>
-
-        <li><p>Фобия:           {fear},</p></li>
-
-        <li><p>Черта характера: {characteristic},</p></li>
-
-        <li><p>Багаж:           {baggage},</p></li>
-
-        <li><p>Доп. инфа:       {personInfo}.</p></li>
-
-        <li><p>"id персонажа":  {bunkerCharacterId},</p></li>
-	</ul>  
-    <html>
-
-        """
     
+    else:
+        return bunker_character_template.render(
+            bunkerId=bunkerId, gender=gender, age=age, profession=profession,
+            profession_exp=profession_exp, hobby=hobby, hobby_exp=hobby_exp,
+            fear=fear, characteristic=characteristic, baggage=baggage, personInfo=personInfo,
+            letOrGod=letOrGod
+            )
+
+    
+def CreateRandomBunker(isJson:bool=False) -> str:
+    size = random.choice([space for space in range(100) if space % 10 == 0 and space > 20])
+    #размер в кв. метров
+    stocks = Bunker("Stock", 2)
+    room_count = random.randint(2,3)
+    rooms = Bunker("Rooms",room_count)
+
+    if isJson:
+        json_file_value = f"""{{ 
+            "size": {size},
+            "stocks": "{stocks}",
+            "room_count": {room_count},
+            "rooms": "{rooms}"
+        }}"""
+        
+        return json.loads(json_file_value)
+    else:
+        return bunker_building_template.render(
+            size=size, stocks=stocks, room_count=room_count, rooms=rooms
+            )
+
+#print( CreateRandomCharacter(isJson=False) )
+#print( CreateRandomBunker(isJson=False) )
+
--- api/index.py
+++ api/index.py
@@ -3,13 +3,12 @@ from fastapi import FastAPI, Request
 from sys import version as python_formatted_version
 from fastapi.responses import HTMLResponse
 from datetime import datetime, timedelta
-from .bunkergame import CreateRandomCharacter
+from .bunkergame import *
 import redis
 
 app = FastAPI()
 
-HTMLdirectory:str = os.path.abspath(os.getcwd()) + "/html/"
-character_output = "Null"
+site_output = "Null"
 
 KV_USERNAME = os.environ.get('KV_USERNAME')
 KV_PASS = os.environ.get('KV_PASS')
@@ -24,42 +23,57 @@ r = redis.Redis(
     ssl=True
 )
 
-@app.get("/d")
+@app.get("/date")
 async def root():
     return {
         "GMT+11 time": format(datetime.utcnow()+timedelta(hours=11))
         }    # make GMT+11
 
-@app.get("/messages")   # GET  <host>/messages?add=value to add
+@app.get("/messages")   # GET  <host>/messages?add=value to add message
 async def r_add(request: Request):
     params = request.query_params
-    time_str = format(datetime.utcnow()+timedelta(hours=11))+" GMT+11"
+
     if 'add' in params:
-        value = str(params['add']).replace("\n"," ")
-        if len(value) > 200:
-            value = value[:200]
-        r.lpush('list_messages', value)   # insert at list begin
-        r.ltrim('list_messages', 0, 50)                 # save only first x elements
-        print(f"added {value}")
-    return {"redis_values": [i.decode("utf-8") for i in r.lrange('list_messages',0,21)] }    
+        message = str(params['add']).replace("\n"," ")
+
+        if len(message) > 200:
+            message = message[:200]
+        r.lpush(str(message))  # insert at list begin
+        r.ltrim('list_messages', 0, 50) # save only first x elements
+
+    return {"redis_values": [i.decode("utf-8") for i in r.lrange('list_messages',0,51)] }    
 
 @app.post("/messages")   # POST
 async def r_post_add(request: Request):
+
     if 'add' in request.headers:
-        time_str = format(datetime.utcnow()+timedelta(hours=11))+" GMT+11"
-        add_value = request.headers.get('add')
-        r.lpush('list_val', time_str+' (POST) '+str(add_value))         # insert at list begin
-        r.ltrim('list_val', 0, 20)                  # save only first x elements
-    return {"redis_values": [i.decode("utf-8") for i in r.lrange('list_val',0,21)] }    
+
+        message = request.headers.get('add')
+
+        if len(message) > 200:
+            message = message[:200]
+        r.lpush(str(message))  # insert at list begin
+        r.ltrim('list_messages', 0, 50) # save only first x elements
+
+    return {"redis_values": [i.decode("utf-8") for i in r.lrange('list_messages',0,51)] }    
 
 
 @app.get("/api/character") #GET (already created) random bunker character 
 async def get_character(request: Request): #<host>/api/character?json
-    global character_output
+    global site_output
     params = request.query_params
     isJson = 'json' in params
-    character_output = CreateRandomCharacter(isJson=isJson)
-    return HTMLResponse(character_output) if not isJson else JSONResponse(character_output)
+    site_output = CreateRandomCharacter(isJson=isJson)
+    return HTMLResponse(site_output) if not isJson else JSONResponse(site_output)
+
+
+@app.get("/api/bunker") #GET (already created) random bunker building
+async def get_bunker(request: Request): #<host>/api/bunker?json
+    global site_output
+    params = request.query_params
+    isJson = 'json' in params
+    site_output = CreateRandomBunker(isJson=isJson)
+    return HTMLResponse(site_output) if not isJson else JSONResponse(site_output)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -67,5 +81,4 @@ async def main():
     with open( HTMLdirectory+"mainSite.html", "r" ) as f:
         return f.read()
     
-
 #@python {str(python_formatted_version)}
